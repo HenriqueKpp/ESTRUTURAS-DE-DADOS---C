45df10bcf0317f4ff928373f4841227dee4ab296
Henrique Dantas Faria <164080273+HenriqueKpp@users.noreply.github.com>
2025-05-06 10:19:20 -0300

    Update listaduplamaenteencadeada.c

--- listaduplamaenteencadeada.c
+++ listaduplamaenteencadeada.c
@@ -76,59 +76,94 @@ novo->valor = valor;
 No *aux=  lista->inicio;
 lista->inicio = novo;
 lista->inicio->posterior = aux;
-lista->inicio->posterior->anterior = lista->inicio->posterior;
+lista->inicio->posterior->anterior = lista->inicio;
 }
 
 
 
 
 
-//void ImprimirListaFinal(Lista *lista){
-//int i = 1;
-//No *aux;
-//aux = lista->inicio;
-//while(aux->posterior != NULL){
-//aux = aux->posterior;
-//i++;
-//}
-//printf("\n\n*********LISTA REVERSA********\n");
-//while(aux->anterior != NULL){
-//printf("%d- %d\n",i,aux->valor);
-//i--;
-//aux = aux->anterior;
-//}
+void ImprimirListaFinal(Lista *lista){
+int i = 1;
+No *aux;
+aux = lista->inicio;
+while(aux->posterior != NULL){
+aux = aux->posterior;
+i++;
+}
+printf("\n\n*********LISTA REVERSA********\n");
+while(aux != NULL){
+printf("%d- %d\n",i,aux->valor);
+i--;
+aux = aux->anterior;
+}
 
-//}
+}
+
+//TERMINANDO ***********
+void InserirNoMeio(Lista *lista){
+int posicao;
+int quantidade =0;
+printf("\n\nEM QUAL POSICAO DE SEJA INSERIR?\n ");
+scanf("%d\n\n",&posicao);
+No *novo = (No*)malloc(sizeof(No));
+No *aux = lista->inicio;
+do{
+if(aux != NULL)
+quantidade++;
+aux = aux->posterior;
+} while(aux!= NULL);
+if(posicao == 1){
 
 
+}
+if(posicao == quantidade){
 
 
+}
+
+}
 
 
 
 int main(){
-
 Lista *lista;
+int ope;
 Criarlista(lista);
-NovoNoFinal(lista);
-NovoNoFinal(lista);
-NovoNoFinal(lista);
-NovoNoFinal(lista);
-NovoNoInicio(lista);
-NovoNoInicio(lista);
-ImprimirListaInicio(lista);
-//nao funciona ainda
-//ImprimirListaFinal(lista);
+do{
 
+printf("\n\nOPERACOES: 1- INSERIR FINAL\t 2- INSERIR INICIO\t 3-IMPRMIR LISTA PELO INICIO\t 4-IMPRIMIR PELO FINAL\n \t5- INSERIR NO MEIO \t 10- FINALIZAR PROGRAMA\n\n");
+scanf("%d",&ope);
 
+switch(ope)
+{
+case 1:
+   NovoNoFinal(lista);
+    break;
+case 2:
+    NovoNoInicio(lista);
+break;
 
+case 3:
+    ImprimirListaInicio(lista);
+break;
 
+case 4:
+    ImprimirListaFinal(lista);
+break;
 
+case 5: 
+    //TERMINANDO******
+    InserirNoMeio(lista);
+    break;
 
+default:
+printf("\n******Valor invalido!******\n");
+    break;
 
+}
 
-
-
+}while(ope!= 10);
 
     return 0;
 }
